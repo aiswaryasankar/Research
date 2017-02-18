0c94a3aaf3d032dd1f00ebe0c0e13f06a8379e80
aiswaryasankar <aiswarya.tennis@gmail.com>
2017-02-17 22:53:00 -0800

    Back propagation update for 2 layers
    Updating inner layers based on the differences between the weights

--- secondNN.py
+++ secondNN.py
@@ -4,7 +4,7 @@
 	# Train will loop over all the layers
 	# Neural network will take in all the layers, each layer will have a matrix of values and weights and then perform the functions
 	# Update will be separate to update the weights
-
+import numpy as np
 
 class Layer(object):
 
@@ -17,52 +17,40 @@ class Layer(object):
 		# I am initializing the arrays randomly
 		return np.dot(X, self.weights)
 
-	def backprop(self):
-		while (count < 50):
-			gradient = (2.0/ len(y) * sum(np.dot(np.dot(X, self.weights) - y, X)))
-			self.update(gradient)
+	def backprop(self, pred, actual):
+		gradient = (2.0 / len(pred) * sum(np.dot(np.dot(pred - actual, pred), pred)))
+		return gradient
 
 	def update(self, gradient):
 		for i in range(len(self.weights)):
-			self.weights[i] = self.weights[i] - 0.01 * gradient
+			self.weights[i] = self.weights[i] - 0.0001 * gradient
+		return self.weights
 
 
 class NeuralNetwork(object):
 
 	def __init__(self, weights):
 		#self.layers = 
-		self.layers = Layer(weights)
-		self.a, self.b = 1, 2
-		self.X = [[m, 1] for m in range(1, 10)]
-		self.Y = [a*m + b for m in range(1, 10)]
+		self.layer1 = Layer(weights[0])
+		self.layer2 = Layer(weights[1])
+		a = weights[0][0]
+		b = weights[0][1]
+		c = weights[1][0]
+		d = weights[1][1]
+		self.X = np.array([[m, 1] for m in range(1, 10)])
+		self.Y = np.array([6 * (4*m + 5) + 7 for m in range(1, 10)])
 		#for layer in weights:
-		#	self.layers.append(Layer(weights))
-
-	def createData(self):
-		a, b, c, d = 1,2,3,4
-		X = [[m, 1] for m in range(1, 10)]
-		y = [c * (a*m + b) + d for m in range(1, 10)]
 
 	def train(self):
 		# I want to iterate across all the layers
 		for layer in self.layers:
 			# For the given layer I want to do a forward iteration and update the X value accordingly
 			self.X = layer.forward(self.X)
-		# After I do all the forward propagation, I need to do backpropagation
-		# For the first layer I have the actual values to subtract from y and x
-
-		# For the second layer I don't have actual y values
-
-		
-		# Now I want to loop through and learn the weights
 		gradient = 10.0
 		count = 0
 		while (count < 50):
 			# Backprop method
 			gradient = (2.0/ len(y) * sum(np.dot(np.dot(X, self.weights) - y, X)))
-			print ('gradient is ')
-			print(gradient)
-			print('weight is ')
 			# Update method
 			for i in range(len(self.weights)):
 				self.weights[i] = self.weights[i] - 0.01 * gradient
@@ -72,7 +60,27 @@ class NeuralNetwork(object):
 #neuron = Neuron(2)
 #neuron.train()
 # weights = [[4, 5], [6,7]]
-weights = [5,6]
-NeuralNetwork(weights)
 
+# Here I want to initialize a Neural Net with 2 layers. The first layer will have the given weights. We will perform forward propagation on the X and the first layer weights.  Then that result with the second layer weights.  I will then compare these results with the actual Y values.
+weights = np.array([[4, 5], [6,7]])
+neuralNet = NeuralNetwork(weights)
+train_weights = np.array([[3,5], [4,8]])
+count = 0
+while count < 50:
+	X_prime = neuralNet.layer1.forward(neuralNet.X)
+# I need to make the X matrix into the same dimensions as before
+	X_2 = np.array([[val, 1] for val in X_prime])
+	X_pred = neuralNet.layer2.forward(X_2)
+
+# Now I need to do backpropagation
+# The first layer is going to be the same.  I will subtract the actual values from the predicted values.
+# What will my method take in?  It should take in the predicted values and the generated values.  These can be the weights computed or the actual Y values
+# Backprop thus takes in two parameters, predction and actual
+
+	diff = neuralNet.layer2.backprop(X_pred, neuralNet.Y)
+	updated_weights = neuralNet.layer2.update(diff)
+	# This has updated the last layer's weights.  Now I will do the first layer's weights.
+	diff2 = neuralNet.layer1.backprop(updated_weights, neuralNet.layer1.weights)
+	updated_weights = neuralNet.layer1.update(diff2)
+	count += 1
 
